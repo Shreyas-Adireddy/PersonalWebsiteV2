fecd270271ef368d941a55c0c119457266975e79
Shreyas-Adireddy <shreyasadireddy@gmail.com>
2023-10-19 11:43:51 -0400

    commit

--- src/data/aboutMeData.js
+++ src/data/aboutMeData.js
@@ -1,6 +1,6 @@
 export const aboutMeData = [
 	{
-		id: 1,
+		id: 2,
 		bio: "Welcome to my personal website! I'm Shreyas and I'm fueled by an unyielding passion for technology and innovation.\
 		 As a computer engineering student, I'm on a journey of constant exploration, diving deep into the realms of software development \
 		 and electrical engineering. With a strong foundation in programming languages like Python and C/C++, I enjoy learning new things \
@@ -8,10 +8,7 @@ export const aboutMeData = [
 		every day! ",
 	},
 	{
-		id: 2,
-		bio: "Collaboration is at the core of my approach, and I thrive in dynamic team environments where diverse ideas converge. I love quantiative analysis \
-		and being involved with engineering related projects where me and my coworkers can contribute something to society. I want to make an impact on people. \
-		My goal is to make a meaningful difference in people's lives. Whether through more efficient processes, user-friendly interfaces, or innovative technologies, I'm driven by the desire to leave a positive imprint on society. I believe that by leveraging technology and innovation, we have the power to shape a brighter future for all.\
-		",
+		id: 1,
+		bio: ""
 	},
 ];
